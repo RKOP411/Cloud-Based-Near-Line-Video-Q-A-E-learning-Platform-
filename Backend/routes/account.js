4bb3521df59ccef7540051b21406bb8424c435b4
RKOP411 <michaelleung6884@gmail.com>
2024-10-23 16:58:15 +0800

    save

--- Backend/routes/account.js
+++ Backend/routes/account.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
+router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
+  });
+  
+
+
 router.post('/register', async function (req, res, next) {
     const { UserName, Email, Password, Role } = req.body;
 
