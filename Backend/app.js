81bd8f7442cbded9d4139021deb53f06916528ac
RKOP411 <michaelleung6884@gmail.com>
2024-10-23 17:52:56 +0800

    save

--- Backend/app.js
+++ Backend/app.js
@@ -7,8 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var accountRouter = require('./routes/account');
-const cors = require('cors'); 
-app.use(cors());
+var cors = require('cors');
 
 var app = express();
 
@@ -21,10 +20,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/account', accountRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
