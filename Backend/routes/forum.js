5c6a27606c67fe8b158fabba34e99630a949e759
LKT <michaelleung6884@gmail.com>
2024-11-14 20:31:04 +0800

    GetCommentByForumID

--- Backend/routes/forum.js
+++ Backend/routes/forum.js
@@ -61,5 +61,39 @@ router.get('/GetforumByID/:forumId', async function (req, res, next) {
 });
 
 
+router.get('/GetCommentByForumID/:forumId', async function (req, res, next) {
+
+    try {
+        const connection = await connectToDB();
+        const forumId = req.params.forumId; // Get the ForumID from the URL parameter
+
+        const sql = `
+            SELECT f.*, u.UserName 
+            FROM ForumComment f 
+            LEFT JOIN User u ON f.UserID = u.UserID 
+            WHERE f.ForumID = ?`;
+
+        connection.query(sql, [forumId], (err, results) => {
+            if (err) {
+                console.error('Error retrieving forum:', err);
+                return res.status(500).json({ error: 'Database error' });
+            }
+
+            if (results.length === 0) {
+                return res.status(404).json({ error: 'Forum not found' });
+            }
+
+            res.status(200).json(results[0]); // Return the first result
+        });
+
+        // Close the connection
+        connection.end();
+
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+        res.status(500).send('Server error');
+    }
+});
+
 
 module.exports = router;
