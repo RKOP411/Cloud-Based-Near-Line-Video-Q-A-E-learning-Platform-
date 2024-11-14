bba11cd279d83db8adf0c44c6c8d4ce560b2061a
LKT <michaelleung6884@gmail.com>
2024-11-14 15:16:47 +0800

    GetAllforum in backend

---
+++ Backend/routes/forum.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+const { connectToDB } = require('../util/db');
+
+/* GET home page. */
+router.get('/GetAllforum', async function(req, res, next) {
+    try {
+        const connection = await connectToDB();
+
+        const sql = `SELECT * FROM Forum`;
+
+        connection.query(sql, (err, results) => {
+            if (err) {
+                console.error('Error getting forum:', err);
+                console.log("Database error");
+            }
+            console.log(results);
+            res.status(200).json(results);
+        });
+
+        // Close the connection
+        connection.end();
+
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+        res.status(500).send('Server error');
+    }
+});
+
+module.exports = router;
