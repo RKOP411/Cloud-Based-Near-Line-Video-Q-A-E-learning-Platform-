356654cf45e32f53ca035ad4e87a4308f2a3e3e2
LKT <michaelleung6884@gmail.com>
2025-04-12 22:23:43 +0800

    save

--- Backend/util/db.js
+++ Backend/util/db.js
@@ -1,19 +1,36 @@
 const mysql = require('mysql2');
 
-// Create a MySQL connection pool
-const pool = mysql.createPool({
-    host: 'localhost', // Your database host
-    user: 'root',      // Your database username
-    password: 'root12', // Your database password
-    database: 'LearnPlatform',
-    waitForConnections: true,
-    connectionLimit: 8, // Adjust this number based on your expected load
-    queueLimit: 0
-});
-
-// Function to get a connection from the pool
+// Connect to MongoDB
 async function connectToDB() {
-    return pool.promise(); // Use promise-based pool for async/await
+    const connection = mysql.createConnection({
+        host: 'localhost', // Your database host
+        user: 'root',      // Your database username
+        password: 'root12', // Your database password
+        database: 'LearnPlatform',
+        multipleStatements: true,
+        waitForConnections: true,
+        connectionLimit: 8, // Adjust this number based on your expected load
+        queueLimit: 0,
+        connectTimeout: 100, // Connection timeout in milliseconds
+        idleTimeout: 1, // Idle timeout in milliseconds
+        keepAliveInitialDelay: 1
+    });
+
+    
+
+    // Connect to MySQL
+    connection.connect((err) => {
+        if (err) {
+            console.error('Error connecting to MySQL:', err.stack);
+            return;
+        }
+        console.log('Connected to MySQL as id ' + connection.threadId);
+    });
+
+    return connection;
+
+
 }
+
 
 module.exports = { connectToDB };
